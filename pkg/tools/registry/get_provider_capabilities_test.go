e4a5a921d0ddfe7177f7178b82e1a8253b045b8c
Manu Chandrasekhar <manuchn@amazon.com>
2025-10-14 15:40:54 -0400

    feat: docs update, added test for time

--- pkg/tools/registry/get_provider_capabilities_test.go
+++ pkg/tools/registry/get_provider_capabilities_test.go
@@ -37,6 +37,42 @@ func TestAnalyzeAndFormatCapabilities(t *testing.T) {
 	}
 }
 
+func TestAnalyzeAndFormatCapabilitiesTimeProvider(t *testing.T) {
+	// Mock time provider docs with limited features compared to AWS
+	mockDocs := client.ProviderDocs{
+		Docs: []client.ProviderDoc{
+			{Category: "resources", Title: "time_offset", Language: "hcl"},
+			{Category: "resources", Title: "time_sleep", Language: "hcl"},
+			{Category: "resources", Title: "time_static", Language: "hcl"},
+			{Category: "functions", Title: "time_add", Language: "hcl"},
+			{Category: "functions", Title: "time_cmp", Language: "hcl"},
+		},
+	}
+
+	result := analyzeAndFormatCapabilities(mockDocs, "hashicorp", "time", "0.13.1")
+
+	// Basic checks
+	if result == "" {
+		t.Error("Expected non-empty result")
+	}
+
+	// Check that it contains expected sections for time provider
+	expectedSections := []string{"Resources: 3", "Functions: 2"}
+	for _, section := range expectedSections {
+		if !contains(result, section) {
+			t.Errorf("Expected result to contain '%s'", section)
+		}
+	}
+
+	// Check that it doesn't contain data-sources or guides (time provider has fewer features)
+	unexpectedSections := []string{"Data Sources:", "Guides:"}
+	for _, section := range unexpectedSections {
+		if contains(result, section) {
+			t.Errorf("Expected result to NOT contain '%s'", section)
+		}
+	}
+}
+
 func contains(s, substr string) bool {
 	return len(s) >= len(substr) && s[:len(substr)] == substr ||
 		(len(s) > len(substr) && contains(s[1:], substr))
