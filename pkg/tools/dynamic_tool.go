4bf1064783745c4f8cdc47bfa196bdd4a5b5d6d0
jaylonmcshan19-x <jaylon.mcshan@hashicorp.com>
2026-01-07 14:17:53 -0600

    add attach_policy_set_to_workspaces tool

--- pkg/tools/dynamic_tool.go
+++ pkg/tools/dynamic_tool.go
@@ -241,6 +241,11 @@ func (r *DynamicToolRegistry) registerTFETools() {
 		r.mcpServer.AddTool(tool.Tool, tool.Handler)
 	}
 
+	if toolsets.IsToolEnabled("attach_policy_set_to_workspaces", r.enabledToolsets) {
+		tool := r.createDynamicTFETool("attach_policy_set_to_workspaces", tfeTools.AttachPolicySetToWorkspaces)
+		r.mcpServer.AddTool(tool.Tool, tool.Handler)
+	}
+
 	// Terraform toolset - Variable tools
 	if toolsets.IsToolEnabled("list_workspace_variables", r.enabledToolsets) {
 		tool := r.createDynamicTFETool("list_workspace_variables", tfeTools.ListWorkspaceVariables)
