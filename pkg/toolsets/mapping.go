ed779b8caae3784f1ea9f61dd105783461a3a050
Jose Marin <111805040+Marin2409@users.noreply.github.com>
2026-08-06 21:17:23 -0500

    Minor Code Changes (Code cleanup)

--- pkg/toolsets/mapping.go
+++ pkg/toolsets/mapping.go
@@ -28,6 +28,7 @@ var ToolToToolset = map[string]string{
 	"list_terraform_projects": Terraform,
 	"create_project":          Terraform,
 	"delete_project":          Terraform,
+	"get_project":             Terraform,
 
 	// Terraform tools - Team
 	"create_team": Terraform,
@@ -70,7 +71,6 @@ var ToolToToolset = map[string]string{
 	"list_state_versions":                 Terraform,
 	"get_state_version":                   Terraform,
 	"get_run_comments":                    Terraform,
-	"get_project":                         Terraform,
 }
 
 // GetToolsetForTool returns the toolset name for a given tool name
