3d8702fc61da15f41e2862f089e894616228316a
Mulya-Patel <mulya.patel@ibm.com>
2026-09-03 12:08:19 -0400

    Add search_private_modules, search_private_providers, and get_private_provider_details to mcp-official

--- pkg/mcp-official/tools/tools.go
+++ pkg/mcp-official/tools/tools.go
@@ -15,4 +15,16 @@ func RegisterTools(svr *mcp.Server, logger *log.Logger, enabledToolsets []string
 	if toolsets.IsToolEnabled("list_terraform_orgs", enabledToolsets) {
 		mcp.AddTool(svr, tfeTools.ListTerraformOrganizationsTool(), tfeTools.ListTerraformOrganizationsFunc)
 	}
+
+	if toolsets.IsToolEnabled("search_private_modules", enabledToolsets) {
+		mcp.AddTool(svr, tfeTools.SearchPrivateModulesTool(), tfeTools.SearchPrivateModulesFunc)
+	}
+
+	if toolsets.IsToolEnabled("search_private_providers", enabledToolsets) {
+		mcp.AddTool(svr, tfeTools.SearchPrivateProvidersTool(), tfeTools.SearchPrivateProvidersFunc)
+	}
+
+	if toolsets.IsToolEnabled("get_private_provider_details", enabledToolsets) {
+		mcp.AddTool(svr, tfeTools.GetPrivateProviderDetailsTool(), tfeTools.GetPrivateProviderDetailsFunc)
+	}
 }
